8be24482bb63c98056f90bdc982e5c399ea9f3ed
Dhruv Karia <dhruvkaria@hotmail.com>
2021-07-02 09:25:45 +0000

    ready for production deployment

--- app/main.py
+++ app/main.py
@@ -14,15 +14,15 @@ from utils import get_base_url, allowed_file, and_syntax
     port may need to be changed if there are multiple flask servers running on same server
     comment out below three lines of code when ready for production deployment
 '''
-port = 12345
-base_url = get_base_url(port)
-app = Flask(__name__, static_url_path=base_url+'static')
+#port = 12345
+#base_url = get_base_url(port)
+#app = Flask(__name__, static_url_path=base_url+'static')
 
 '''
     cv scaffold code
     uncomment below line when ready for production deployment
 '''
-# app = Flask(__name__)
+app = Flask(__name__)
 
 UPLOAD_FOLDER = 'images'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -39,13 +39,26 @@ cfg_path = os.path.join('yolo', 'yolo.cfg')
 net = get_yolo_net(cfg_path, weights_path)
 
 
-#@app.route('/')
-@app.route(base_url)
+@app.route('/')
+#@app.route(base_url)
 def home():
     return render_template('home.html')
 
-#@app.route('/', methods=['POST'])
-@app.route(base_url, methods=['POST'])
+@app.route('/overview.html')
+#@app.route(base_url + '/overview.html')
+def overview():
+    return render_template('overview.html')
+
+@app.route('/timeline.html')
+#@app.route(base_url + '/timeline.html')
+def timeline():
+    return render_template('timeline.html')
+
+
+
+
+@app.route('/', methods=['POST'])
+#@app.route(base_url, methods=['POST'])
 def home_post():
     # check if the post request has the file part
     if 'file' not in request.files:
@@ -65,8 +78,8 @@ def home_post():
         return redirect(url_for('results', filename=filename))
 
 
-#@app.route('/uploads/<filename>')
-@app.route(base_url + '/uploads/<filename>')
+@app.route('/uploads/<filename>')
+#@app.route(base_url + '/uploads/<filename>')
 def results(filename): 
     image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     image = cv2.imread(image_path)
@@ -88,25 +101,26 @@ def results(filename):
         labels = set(labels)
         labels = [plant.capitalize() for plant in labels]
         labels = and_syntax(labels)
-        return render_template('results.html', confidences=format_confidences, labels=labels, 
+        return render_template('results1.html', confidences=format_confidences, labels=labels, 
             old_filename = filename, 
             filename=new_filename) 
     else:
         found = False
         # replace 'Objects' with whatever you are trying to detect
-        return render_template('results.html', labels='No Objects', old_filename=filename, filename=filename) 
+        return render_template('results1.html', labels='No Objects', old_filename=filename, filename=filename) 
 
-#@app.route('/files/<path:filename>')
-@app.route(base_url + '/files/<path:filename>')
+@app.route('/files/<path:filename>')
+#@app.route(base_url + '/files/<path:filename>')
 def files(filename):
     return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
+
 
 if __name__ == "__main__":
     '''
     coding center code
     '''
     # IMPORTANT: change the cocalcx.ai-camp.org to the site where you are editing this file.
-    website_url = 'cocalcx.ai-camp.org'
+    website_url = 'cocalc7.ai-camp.org'
     print(f"Try to open\n\n    https://{website_url}" + base_url + '\n\n')
 
     # remove debug=True when deploying it
@@ -118,3 +132,5 @@ if __name__ == "__main__":
     '''
     # Only for debugging while developing
     # app.run(port=80, debug=True)
+    
+    
